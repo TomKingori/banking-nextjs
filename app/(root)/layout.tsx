9627d2b8793afee97b6b127f9334288d5c2b2d66
Tom Kingori <87486793+TomKingori@users.noreply.github.com>
2024-05-13 15:49:13 +0300

    sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import Sidebar from '@/components/Sidebar'
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {firstname: 'Tom', lastname: 'King'} ;
   return (
-    <main>
-       SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+       <Sidebar user={loggedIn} />
        {children}
     </main>
   );
